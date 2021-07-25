316d21caf9336ae64ee1309a11eac5e72dfd18b8
Michael Nowak <michael@nowakphd.com>
2021-07-25 14:25:38 -0500

    updates to support visual debugging

--- .config/provision.sh
+++ .config/provision.sh
@@ -10,11 +10,11 @@ apt-get $APT_FLAGS install curl sqlite3
 
 # install and configure clang-10, libc++abi-dev, and libc++-dev
 apt-get $APT_FLAGS install clang-10 libc++abi-dev libc++-dev clangd-10 llvm-10-dev liblldb-10-dev
-ln -nsf /usr/bin/clang-10 /usr/bin/clang
-ln -nsf /usr/bin/clang++-10 /usr/bin/clang++
-ln -nsf /usr/bin/lldb-10 /usr/bin/lldb
-ln -nsf /usr/bin/clangd-10 /usr/bin/clangd
-ln -nsf /usr/bin/lldb-server-10 /usr/bin/lldb-server-10.0.0
+ln -sf /usr/bin/clang-10 /usr/bin/clang
+ln -sf /usr/bin/clang++-10 /usr/bin/clang++
+ln -sf /usr/bin/lldb-10 /usr/bin/lldb
+ln -sf /usr/bin/clangd-10 /usr/bin/clangd
+ln -sf /usr/bin/lldb-server-10 /usr/bin/lldb-server-10.0.0
 # update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 100
 # update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 100
 # update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-10 100
@@ -22,8 +22,8 @@ ln -nsf /usr/bin/lldb-server-10 /usr/bin/lldb-server-10.0.0
 
 # other utilities
 apt-get $APT_FLAGS install clang-format-10 clang-tidy-10 cppcheck uncrustify
-ln -nsf /usr/bin/clang-format-10 /usr/bin/clang-format
-ln -nsf /usr/bin/clang-tidy-10 /usr/bin/clang-tidy
+ln -sf /usr/bin/clang-format-10 /usr/bin/clang-format
+ln -sf /usr/bin/clang-tidy-10 /usr/bin/clang-tidy
 # update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-10 100
 # update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-10 100
 
@@ -35,6 +35,9 @@ cd lldb-mi
 cmake .
 cmake --build .
 mv src/lldb-mi /usr/bin/
+# add bin to home folder for visual debugging
+mkdir /home/vagrant/bin/
+sudo chown -R vagrant:vagrant /home/vagrant/bin/
 
 # other configuration
 sudo ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
